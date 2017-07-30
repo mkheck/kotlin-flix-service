c8f3e5bf21d9711093231f4e4507def6d346ac47
Mark Heckler <mark.heckler@gmail.com>
2017-07-30 10:40:13 -0500

    Updated gitignore & code

--- src/main/kotlin/com/example/kotlinflixservice/DemoData.kt
+++ src/main/kotlin/com/example/kotlinflixservice/DemoData.kt
@@ -3,16 +3,17 @@ package com.example.kotlinflixservice
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
 import java.util.*
+import javax.annotation.PostConstruct
 
 @Component
 class DemoData(val repo: MovieRepository) {
+    @PostConstruct
     fun run() {
-        repo.deleteAll().block()
-
-        Flux.just("Enter the Mono<Void>", "Y Tu Mono Tambien")
-                .map { title -> Movie(UUID.randomUUID().toString(), title) }
-                .flatMap { t: Movie? -> repo.save(t)}
-                .subscribe { t: Movie? -> println(t)}
-//                .subscribe(System.out::println)
+        repo.deleteAll()
+                .thenMany(
+                        Flux.just("Enter the Mono<Void>", "Y Tu Mono Tambien")
+                                .map { title -> Movie(UUID.randomUUID().toString(), title) }
+                                .flatMap { t -> repo.save(t) })
+                .subscribe { t -> println(t) }
     }
 }
